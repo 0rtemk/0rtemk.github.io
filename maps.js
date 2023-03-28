9a1b1e8c89d84aef514c07a27dc99a902d960934
0rtemk <lakeylive@yandex.ru>
2023-03-28 18:32:47 +0300

    Implement the second level

---
+++ maps.js
@@ -0,0 +1,26 @@
+ymaps.ready(init);
+
+function init() {
+    let myMap = new ymaps.Map("map", {
+        center: [61.785020, 34.346878],
+        zoom: 12,
+        controls: [
+            'zoomControl',
+            'rulerControl',
+            'fullscreenControl',
+            new ymaps.control.SearchControl({
+                options: {
+                    size: 'large',
+                    provider: 'yandex#search'
+                }
+            })
+        ]
+    });
+
+    let myPlacemark = new ymaps.Placemark([61.791257, 34.364545], {
+        hintContent: 'Место жительства',
+        balloonContent: 'Я тут живу, да'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
