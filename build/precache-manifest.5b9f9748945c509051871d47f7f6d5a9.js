87a0ad8ce961630626b32acb99b6e6f18a0037a7
0rtemk <sarte@Kavbanga.ru>
2025-09-24 19:49:28 +0300

    initial

---
+++ build/precache-manifest.5b9f9748945c509051871d47f7f6d5a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b994ff2840f1a168e0cfd36f1a9e8361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a7c7b9658f9829bd828",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "9a7c7b9658f9829bd828",
+    "url": "/static/js/main.51645289.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import React, { useState } from "react";
+import Field from "./components/field";
+import Languages from "./components/languages";
+import Translate from "./components/translate";
+
+export default function App() {
+  const [language, setLanguage] = useState("en");
+  const [text, setText] = useState("Реакция");
+
+  return (
+    <div>
+      <Field label="Введите русские слова" onChange={setText} value={text} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
+    </div>
+  );
+}
